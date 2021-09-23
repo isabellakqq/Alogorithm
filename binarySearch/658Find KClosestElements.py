bdc01b91fe99fb2ee161c6c34f117ce72abbd9e7
qingqingkang <isabellakqq@gmail.com>
2021-09-22 22:54:02 -0700

    Binary Search Fackbook 面经

---
+++ binarySearch/658Find KClosestElements.py
@@ -0,0 +1,21 @@
+import bisect
+def findClosestelement(arr, k, x):
+    index = bisect.bisect_right(arr, x)
+    left = index - 1
+    right = index
+    for _ in range(k):
+        if is_left_closer(arr, left, right, x):
+            left -= 1
+        else:
+            right += 1
+    return arr[left + 1 : right]
+
+def is_left_closer(arr, left, right, x):
+    if right >= len(arr):
+        return True
+    if left < 0:
+        return False
+    return abs(arr[left] - x) <= abs(arr[right] - x)
+
+print(findClosestelement([1, 2, 3, 4, 5], 2, 0))
+
